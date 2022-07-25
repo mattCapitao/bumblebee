74ba5a02ca290dc62d9f96e82c5142bff1b9482d
mattCapitao <matt.capitao@gmail.com>
2022-07-25 17:02:40 -0400

    Adds rain sound when raincloud spawns

--- src/js/app.js
+++ src/js/app.js
@@ -331,7 +331,7 @@ window.setInterval(function () {
       );
 
       if (Game.fadeRainSound == true) {
-        Game.rainSound.volume -= 0.0005;
+        Game.rainSound.volume -= 0.05;
         if (Game.rainSound.volume <= 0) {
           Game.rainSound.pause();
           Game.rainSound = null;
